105bdc91d978c7c0c99478afacb81a3d035f5ca1
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-09-16 00:39:45 +0300

    Update exampl.js

--- js/exampl.js
+++ js/exampl.js
@@ -1,3 +1,21 @@
-let camper = "James";
- camper = "David";
-console.log(camper);
+// function checkAge(age) {
+//   if (age >= 18) { // Change this line
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+// const fruits = ["apple", "plum", "pear", "orange"];
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits.length - 1;
+// console.log(firstElement);
+// console.log(secondElement);
+// console.log(fruits[lastElement]);
+const fruits = ["apple", "peach", "pear", "banana"];
+const fruitsArrayLength = fruits.length;
+console.log(fruitsArrayLength)
