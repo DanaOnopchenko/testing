989ee7c34f29b07bfe6d630bb7c80575c47947d6
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-09-21 03:15:26 +0300

    Update exampl.js

--- js/exampl.js
+++ js/exampl.js
@@ -1,143 +1,28 @@
-// function checkFruit(fruit) {
-//   const fruits = ["apple", "plum", "pear", "orange"];
-
-//   return fruits.includes(fruit); // Change this line
-// }
-
-// console.log(checkFruit("plum"));
-// console.log(checkFruit("mandarin"));
-// console.log(checkFruit("pear"));
-// console.log(checkFruit("Pear"));
-// console.log(checkFruit("apple"))
-
-// function getCommonElements(array1, array2) {
+// function includes(array, value) {
 //   // Change code below this line
-//     let newElements = [];
-//     for (let i = 0; i < array1.length; i += 1) {
-//         let newArray = array1[i];
-//         if (array2.includes(newArray))
-//             newElements.push(newArray)
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (array[i] === value)
+//       return true;
 //     }
-      
-// return newElements
-//  // Change code above this line
-// }
-// console.log(getCommonElements([1, 2, 3], [2, 4]));
-// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
-// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
-// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
-// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
-
-// function calculateTotalPrice(order) {
-//   let total = 0;
-//   // Change code below this line
-//     let totalPrices = order;
-//     for (const totalPrice of totalPrices) {
-//         total += totalPrice;
-//     }
-// //   for (let i = 0; i < order.length; i += 1) {
-// //     total += order[i];
-// //   }
-
-//   // Change code above this line
-//   return total;
-// }
-// console.log(calculateTotalPrice([12, 85, 37, 4]));
-// console.log(calculateTotalPrice([164, 48, 291]));
-// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
-// console.log(calculateTotalPrice([]));
-
-// function filterArray(numbers, value) {
-//   // Change code below this line
-//     const filteredNumbers = [];
-   
-//     for (let number of numbers) {
-//         if (number > value) {
-//             filteredNumbers.push(number);
-
-//         }
-//     }
-//   return filteredNumbers;
-//   // Change code above this line
-// }
-// console.log(filterArray([1, 2, 3, 4, 5], 3));
-// console.log(filterArray([1, 2, 3, 4, 5], 4));
-// console.log(filterArray([1, 2, 3, 4, 5], 5));
-// console.log(filterArray([12, 24, 8, 41, 76], 38));
-// console.log(filterArray([12, 24, 8, 41, 76], 20));
-
-// const a = 3 % 1;
-// const b = 4 % 3;
-// const c = 11 % 4;
-// const d = 12 % 7;
-// const e = 8 % 6;
-
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log(d);
-// console.log(e);
-
-// function getEvenNumbers(start, end) {
-//    // Change code below this line
-//   let findNumbers = [];
-//   for (let i = start; i <= end; i += 1) {
-//     if (i % 2 === 0) {
-//       findNumbers.push(i)
-//     }
+//   return false;
 //   }
-
-// return findNumbers
-//     // Change code above this line
-// }
   
-// console.log(getEvenNumbers(2, 5));
-// console.log(getEvenNumbers(3, 11));
-// console.log(getEvenNumbers(6, 12));
-// console.log(getEvenNumbers(8, 8));
-// console.log(getEvenNumbers(7, 7))
+// console.log(includes([1, 2, 3, 4, 5], 3));
+// console.log(includes([1, 2, 3, 4, 5], 17));
+// console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+// console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+// console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+// console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
 
-// const start = 6;
-// const end = 27;
-// let number;
-
-// for (let i = start; i < end; i += 1) {
-//   if (i % 5 === 0) {
-//     number = i;
-//     break;
-//   }
-// }
-// console.log(number)
-
-// function findNumber(start, end, divisor) {
-//   // Change code below this line
-//   let number;
-
-//   for (let i = start; i < end; i += 1) {
-//     if (i % divisor === 0) {
-//       number = i;
-//       return i;
-//     }
-//   }
-
-//   return number;
-//   // Change code above this line
-
-// }
-// console.log(findNumber(2, 6, 5));
-// console.log(findNumber(8, 17, 3));
-// console.log(findNumber(6, 9, 4));
-// console.log(findNumber(16, 35, 7))
 
 function includes(array, value) {
   // Change code below this line
-  for (let i = 0; i < array.length; i +=1) { 
-    if (array[i]= value) 
-      return true
-    }
-    return false
+  for (let i = 0; i < array.length; i += 1) { 
+    if (array[i] === value) return true;
   }
+  return false;
   // Change code above this line
+}
 
 console.log(includes([1, 2, 3, 4, 5], 3));
 console.log(includes([1, 2, 3, 4, 5], 17));
