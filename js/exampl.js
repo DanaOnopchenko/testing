73a95455f4f3d914bfa53d74b837eb8c2cfa64ed
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-09-22 03:38:50 +0300

    Update exampl.js

--- js/exampl.js
+++ js/exampl.js
@@ -15,14 +15,51 @@
 // console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
 
 
-function includes(array, value) {
+// function includes(array, value) {
+//   // Change code below this line
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (array[i] === value) return true;
+//   }
+//   return false;
+//   // Change code above this line
+// }
+
+// console.log(includes([1, 2, 3, 4, 5], 3));
+// console.log(includes([1, 2, 3, 4, 5], 17));
+
+
+
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+//   // Change code above this line
+// }
+// console.log(keys);
+// console.log(values);
+
+
+function countProps(object) {
+  let propCount = 0;
   // Change code below this line
-  for (let i = 0; i < array.length; i += 1) { 
-    if (array[i] === value) return true;
+  for (const key in object) { 
+    propCount += key;
   }
-  return false;
   // Change code above this line
+  return propCount;
 }
 
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes([1, 2, 3, 4, 5], 17));
+
+console.log(countProps({ name: "Mango", age: 2 }))
