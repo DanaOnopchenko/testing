089ccfa1a32db3573e703461501f1cd55bec9b83
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-09-24 01:00:51 +0300

    Update exampl.js

--- js/exampl.js
+++ js/exampl.js
@@ -76,41 +76,47 @@
 // const keys = Object.keys(apartment);
 
 // for (const key of keys) {
-//   values.push(apartment(key));
+//   values.push(apartment[key]);
 // }
-// console.log(keys)
+// console.log(keys);
+// console.log(values)
 
-
-
-// const option = 5;
-// let message = '';
-// switch (option) {
-//   case 1:
-//     message = "Ви зможете забрати товар, завтра у 11.00"
-//     break;
-//   case 2:
-//     message = "Кур'єр доставить ваше замовлення завтра"
-//     break;
-//   case 3:
-//     message = "посилка буде відправлена завтра"
-//     break;
-//   default:
-//     message = "вам передзвонять"
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+//   for (const key of keys) {
+//     propCount += 1;
+//   }
+//   return propCount;
+//   // Change code above this line
 // }
 
-// console.log(message)
+// console.log(countProps({ name: "Mango", age: 2 }));
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }))
 
-// function plusThree(num) {
-//   return num + 3;
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+// Change code below this line
+const keys = Object.keys(apartment);
+const values = Object.values(apartment);
+console.log(keys);
+console.log(values);
+
+
+
+
+
+
+
+// function timesFive(num) { 
+//   const answer = num * 5;
+//   return answer;
 // }
 
-// const answer = plusThree(5);
-// console.log(answer)
-function timesFive(num) { 
-  const answer = num * 5;
-  return answer;
-}
 
 
-
-console.log(timesFive(5));
+// console.log(timesFive(5));
