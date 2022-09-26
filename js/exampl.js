99074df5270ccc986629013ed833dd74a69dcd6e
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-09-27 02:21:39 +0300

    Update exampl.js

--- js/exampl.js
+++ js/exampl.js
@@ -378,10 +378,18 @@
 // console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
 
 // Change code below this line
-function findMatches() {
-  const matches = []; // Don't change this line
-
+function findMatches(firstNums,...secondNums) {
+  const matches = [];
+  // Don't change this line
+  for (const el of firstNums){
+     if (secondNums.includes(el)) { 
+    matches.push(el)
+  }
+  }
+ 
   // Change code above this line
   return matches;
 }
-findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7)
+console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
+console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41))
