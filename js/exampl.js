c6a691df284b163ad88a810c05860f2b0bc04262
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-09-28 01:21:01 +0300

    Update exampl.js

--- js/exampl.js
+++ js/exampl.js
@@ -378,18 +378,41 @@
 // console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
 
 // Change code below this line
-function findMatches(firstNums,...secondNums) {
-  const matches = [];
-  // Don't change this line
-  for (const el of firstNums){
-     if (secondNums.includes(el)) { 
-    matches.push(el)
-  }
-  }
+// function findMatches(firstNums,...secondNums) {
+//   const matches = [];
+//   // Don't change this line
+//   for (const el of firstNums){
+//      if (secondNums.includes(el)) {
+//     matches.push(el)
+//   }
+//   }
  
+//   // Change code above this line
+//   return matches;
+// }
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+// console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
+// console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41))
+
+
+const bookShelf = {
+  // Change code below this line
+  books: ["The last kingdom", "The guardian of dreams"],
+  getBooks() {
+    return "Returning all books";
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Deleting book ${bookName}`;
+  },
+  updateBook(oldName, newName) { 
+    return `Updating book ${oldName} to ${newName}`;
+  }
   // Change code above this line
-  return matches;
-}
-console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
-console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
-console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41))
+};
+console.log(bookShelf.addBook("Haze"));
+console.log(bookShelf.removeBook("Red sunset"));
+console.log(bookShelf.updateBook("Sands of dune", "Dune"))
+
