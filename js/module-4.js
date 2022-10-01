5807fcafd4a42843a8479c1b489d732c36fd0606
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-10-02 00:19:36 +0300

    Update module-4.js

--- js/module-4.js
+++ js/module-4.js
@@ -279,135 +279,18 @@
 //   };
 //   // Change code above this line
 
-  const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
-];
 
-
-
-// Task 18
+// Task 22
 // Change code below this line
-// const getUserEmails = users => {
-// const allUsersEmails = users.map(user => user.email);
-//  return allUsersEmails
+const getUsersWithEyeColor = (users, color) => {
 
-// };
-  
-// console.log(getUserEmails)
+ getUsersWithEyeColor = users.filter(user => user.eyeColor === color)
 
+};
+// Change code above this line
 
-// Task 19
-// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
-// // Change code below this line
+console.log(getUsersWithEyeColor(users, "blue"))
 
-// const evenNumbers = numbers.filter(number => number % 2 === 0);
-// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+color = "blue";
 
-// console.log(evenNumbers)
-// console.log(oddNumbers)
-
-
-
-//  Task 20
-
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    genres: ["adventure", "history"],
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    genres: ["fiction", "mysticism"],
-  },
-  {
-    title: "Redder Than Blood",
-    author: "Tanith Lee",
-    genres: ["horror", "mysticism", "adventure"],
-  },
-];
-// Change code below this line
-const allGenres = books.flatMap(book => book.genres);
-
-const uniqueGenres = allGenres.filter((course, index, array) => array.indexOf(course) === index);
-
-console.log(allGenres)
-console.log(uniqueGenres)
  
