eff009bd6bbefbef54e1e4bd8097ba7ce3914878
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-10-04 02:33:13 +0300

    summ

---
+++ js/codewars.js
@@ -0,0 +1,49 @@
+// Task 1
+
+// const allWords = ['hello', 'world', 'this', 'is', 'great'];
+ 
+// const smash = (words) =>
+//     words.join(" ");
+      
+// console.log(smash(allWords));
+
+// Task 2
+
+// function greet (name, owner) {
+//     if (name === owner) {
+//         return `Hello boss`
+//     }
+//     return `Hello guest`
+// }
+// console.log(greet("Anny", "Dan")
+
+// let arr = [1, 2, 3, 4, 5, 3, 2];
+// arr.reverce()
+// console.log(arr)
+
+// let str = '123456789';
+
+// let arr1 = str.split('');
+// let arr2 = arr1.reverse();
+// console.log(arr2);
+// function digitize(n) {
+    
+//     let arr = [];
+//     for (let i = 0)
+//         // .split('')
+//         // .reverse();
+//     return str
+
+//   //code here
+// }
+
+console.log(digitize(11,13,14))
+
+// function digitize(n) {
+//   let arr = [];
+//   for (let i = 0; i < n.length; i += 1) {
+//     arr.push(n[i]);
+//   }
+//   return arr;
+// }
+// console.log(digitize(1235))
