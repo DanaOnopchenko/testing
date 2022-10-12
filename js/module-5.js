a2091a5a0b3c6f9a9c08df8009bfe531ead8081e
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-10-13 01:19:24 +0300

    Update module-5.js

--- js/module-5.js
+++ js/module-5.js
@@ -190,27 +190,58 @@
 // }
 
 // Task 10
-class Storage { 
-  constructor(items) { 
-    this.items = items;  
-  }
-  getItems() { 
-    return this.items;
-  }
-  addItem(newItem) { 
-   this.items.push(newItem)
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//    this.items.push(newItem)
 
+//   }
+//   removeItem(itemToRemove) {
+//     this.items.splice(this.items.indexOf(itemToRemove), 1);
+//      }
+// }
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// task 11
+class StringBuilder { 
+  constructor(vinitialValue) { 
+    this.vinitialValue = vinitialValue;
+    this.value = vinitialValue;
   }
-  removeItem(itemToRemove) { 
-    this.items.splice(this.items.indexOf(itemToRemove), 1);
-     }
+  getValue() { 
+    return this.value;
+  }
+  padEnd(str) { 
+    this.value = this.value + str;
+  }
+  padStart(str) { 
+    this.value = str + this.value ;
+  }
+  padBoth(str) { 
+    this.value = str + this.value + str;
+  }
 }
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
 
 
