ce7d0401a856e446071c6ef8d24d88a4b04111f3
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-01-26 01:05:35 +0200

    update

--- js/object.js
+++ js/object.js
@@ -3,10 +3,31 @@ const playlist = {
   rating: 5,
   tracks: ['трек-1', 'трек-2', 'трек-3'],
   trackCount: 3,
+  changeName(newName) { 
+    console.log(" this in changeName", this);
+    this.name = newName;
+  },
+  addTrack(track) { 
+    this.tracks.push(track);
+    this.trackCount = this.tracks.length;
+  },
+  updateRating(newRating) { 
+    this.rating = newRating;
+  },
 };
-console.log(playlist.tracks);
-console.log(playlist['rating']);
 
-playlist.tracks.push("Mango");
+
+// console.log(playlist.tracks);
+// console.log(playlist['rating']);
+
+// playlist.tracks.push("Mango");
+
 console.log(playlist);
-
+playlist.changeName("new Name");
+console.log(playlist);
+playlist.addTrack('New track');
+console.log(playlist);
+playlist.updateRating(8);
+console.log(playlist);
+playlist.addTrack('Mango');
+console.log(playlist);
