6437d707a90cf8a43c7f579fa2eaeedff8d78282
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-01-28 01:24:18 +0200

    update

--- js/object.js
+++ js/object.js
@@ -1,20 +1,20 @@
-const playlist = {
-  name: 'Мой супер плейлист',
-  rating: 5,
-  tracks: ['трек-1', 'трек-2', 'трек-3'],
-  trackCount: 3,
-  changeName(newName) { 
-    console.log(" this in changeName", this);
-    this.name = newName;
-  },
-  addTrack(track) { 
-    this.tracks.push(track);
-    this.trackCount = this.tracks.length;
-  },
-  updateRating(newRating) { 
-    this.rating = newRating;
-  },
-};
+// const playlist = {
+//   name: 'Мой супер плейлист',
+//   rating: 5,
+//   tracks: ['трек-1', 'трек-2', 'трек-3'],
+//   trackCount: 3,
+//   changeName(newName) { 
+//     console.log(" this in changeName", this);
+//     this.name = newName;
+//   },
+//   addTrack(track) { 
+//     this.tracks.push(track);
+//     this.trackCount = this.tracks.length;
+//   },
+//   updateRating(newRating) { 
+//     this.rating = newRating;
+//   },
+// };
 
 
 // console.log(playlist.tracks);
@@ -22,12 +22,40 @@ const playlist = {
 
 // playlist.tracks.push("Mango");
 
-console.log(playlist);
-playlist.changeName("new Name");
-console.log(playlist);
-playlist.addTrack('New track');
-console.log(playlist);
-playlist.updateRating(8);
-console.log(playlist);
-playlist.addTrack('Mango');
-console.log(playlist);
+// console.log(playlist);
+// playlist.changeName("new Name");
+// console.log(playlist);
+// playlist.addTrack('New track');
+// console.log(playlist);
+// playlist.updateRating(8);
+// console.log(playlist);
+// playlist.addTrack('Mango');
+// console.log(playlist);
+
+
+/*
+ * Перебор через for...in и Object.keys|values|entries
+ */
+
+const feedback = {
+  good: 5,
+  neutral: 10,
+  bad: 3,
+};
+
+const keys = Object.keys(feedback);
+console.log(keys);
+
+for (const key of keys) { 
+  console.log(feedback[key]);
+}
+
+let totalFeadback = 0;
+
+
+const values = Object.values(feedback);
+console.log(values);
+
+for (const value of values) { 
+  console.log(value);
+}
