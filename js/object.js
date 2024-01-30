e5343d9d96f7364a3d54fc0f3026d060c6ae21f0
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-01-31 00:34:18 +0200

    update

--- js/object.js
+++ js/object.js
@@ -266,27 +266,117 @@
  * - Имя переменной отличное от имени свойства
  */
 
-const playlist = {
-  name: 'Мой супер плейлист',
-  rating: 5,
-  tracks: ['трек-1', 'трек-2', 'трек-3'],
-  trackCount: 3,
-};
+// const playlist = {
+//   name: 'Мой супер плейлист',
+//   rating: 5,
+//   tracks: ['трек-1', 'трек-2', 'трек-3'],
+//   trackCount: 3,
+// };
 
-const {
-  rating,
-  tracks,
-  name,
-  trackCount: numberOfTracks = 0,
-  author = 'user',
-} = playlist;
+// const {
+//   rating,
+//   tracks,
+//   name,
+//   trackCount: numberOfTracks = 0,
+//   author = 'user',
+// } = playlist;
 
-console.log(rating, tracks, name);
+// console.log(rating, tracks, name);
 
 
 /*
  * Глубокая деструктуризация
  */
+
+// const profile = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: 'Ocho Rios, Jamaica',
+//   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+// const { name, tag, location, avatar, stats:{followers, views, likes}
+// } = profile;
+
+// console.log(name, tag, location, avatar, followers, views, likes);
+// =====================================================================
+/*
+ * Деструктуризация массивов
+ */
+// const rgb = [255, 100, 80];
+// // const [a, b, c] = rgb;
+// const [red, ,blue] = rgb;
+// // console.log(a,b,c);
+// console.log(red,blue);
+
+
+// =================================
+// const authors = {
+//   kiwi: 4,
+//   poly: 7,
+//   ajax: 9,
+//   mango: 6,
+// };
+// const keys = Object.keys(authors);
+
+// for (const key of keys) {
+//   console.log(key);
+//   console.log(authors[key]);
+// }
+
+// const entries = Object.entries(authors);
+
+// for (const entry of entries) {
+//   // console.log(entry);
+
+//   const [name, rating] = entry;
+//   // const name = entry[0];
+//   // const rating = entry[1];
+//   console.log(name,rating);
+// }
+
+// for (const [name, rating] of entries) {
+//   console.log(name,rating);
+// }
+// ================================================
+
+/*
+ * Операция rest (сбор)
+ */
+
+// const profile = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: 'Ocho Rios, Jamaica',
+//   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// const { name, tag, location,...restProps } = profile;
+
+// console.log(name, tag, location);
+// console.log(restProps);
+
+
+/*
+ * Паттерн «Обьект настроек»
+ * - деструктуризация параметра-обьекта в подписи функции
+ * - rest при деструктуризации в подписи
+ */
+
+
+const showProfileInfo = function ({ name,tag,location,avatar,stats} ) { 
+  // const { name, tag, location, avatar,stats } = userProfile;
+console.log(name,tag,location);
+}
 
 const profile = {
   name: 'Jacques Gluke',
@@ -299,3 +389,5 @@ const profile = {
     likes: 1308,
   },
 };
+
+showProfileInfo(profile)
