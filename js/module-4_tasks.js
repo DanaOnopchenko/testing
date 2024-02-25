a83112b1f43fecc25d72d3386bb4fd93ee8699f4
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-02-26 01:46:09 +0200

    update tasks

--- js/module-4_tasks.js
+++ js/module-4_tasks.js
@@ -496,8 +496,107 @@
 
 // == Task 24 ============
 
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+]
+
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) =>
+//     users.filter(user => user.friends.includes(friendName));
+    
+//       ;
+// // Change code above this line
+
+// console.log(getUsersWithFriend(users,"Goldie Gentry"));
+
+// === Task 25 ===================
+
 // Change code below this line
-const getUsersWithFriend = (users, friendName) => {
+// const getFriends = (users) => {
+//     return users.flatMap(user => user.friends).filter((friend, idx, arr) => arr.indexOf(friend) === idx);
+// }
+    
    
-};
+// ;
+// // Change code above this line
+// console.log(getFriends(users));
+
+// === Task 26 ==========
+
+// Change code below this line
+const getActiveUsers = (users) => { 
+   return  users.filter(({ isActive }) => isActive).filter(user=>user.isActive);
+
+}
+    // users.filter(user=>user.isActive);
 // Change code above this line
+console.log(getActiveUsers(users));
