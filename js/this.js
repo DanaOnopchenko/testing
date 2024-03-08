8472567fd80996b6e45a28f7fbe9367548d3e9aa
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2024-03-08 02:25:10 +0200

    update

--- js/this.js
+++ js/this.js
@@ -97,21 +97,86 @@
 // =================================
 
 
-const makeChangeColor = function () {
-    const changeColor = function (color) {
-        console.log('changeColor -> this', this);
-    };
+// const makeChangeColor = function () {
+//     const changeColor = function (color) {
+//         console.log('changeColor -> this', this);
+//     };
 
-    return changeColor;
+//     return changeColor;
+// };
+
+// const updateColor = makeChangeColor();
+
+// updateColor('yellow'); // Какой this ???
+
+// const hat = {
+//     color: 'blue',
+//     updateColor: updateColor,
+// };
+
+// hat.updateColor('orange'); // Какой this ???
+// ==========================================
+
+const counter = {
+    value: 0,
+    increment(value) {
+        console.log('increment -> this', this);
+        this.value += value;
+    },
+    decrement(value) {
+        console.log('decrement -> this', this);
+        this.value -= value;
+    },
 };
 
-const updateColor = makeChangeColor();
-
-updateColor('yellow'); // Какой this ???
-
-const hat = {
-    color: 'blue',
-    updateColor: updateColor,
+const updateCounter = function (value, operation) {
+    operation(value);
 };
 
-hat.updateColor('orange'); // Какой this ???
+updateCounter(10, counter.increment.bind(counter));
+updateCounter(5, counter.decrement);
+
+/*
+ * call и apply
+ */
+// const showThis = function (a, b, arr) {
+//     console.log(a, b, arr);
+//     console.log('showThis -> this', this);
+// };
+// // console.dir(showThis);
+// showThis();
+
+// const objA = {
+//     a: 5,
+//     b: 10,
+// };
+// showThis.call(objA,10,20,30,)
+// =====================================
+
+//   const changeColor= function(color) { 
+//         console.log('changecolor-> this', this);
+//         this.color = color;
+// };
+
+// const hat = { 
+//     color: 'black',
+   
+// }
+
+// changeColor.call(hat, 'pink');
+// console.log(hat);
+
+
+// const sweater = {
+//     color: 'green',
+  
+// }
+// // changeColor.call(sweater, 'blue');
+// // console.log(sweater);
+// const changeHatColor = changeColor.bind(hat);
+// const changeSweaterColor = changeColor.bind(sweater)
+
+// changeHatColor('pink');
+// console.log(hat);
+// changeSweaterColor('yellow');
+// console.log(sweater);
